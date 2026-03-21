93f19664fa58572112276bca19192e6cac9bd52b
Oliver Studer <oliver.studer@tegonal.com>
2026-03-21 22:55:38 +0100

    Add dashboard stat grid and top projects components

---
+++ frontend_rr7/app/features/dashboard/components/stats-overview-grid.tsx
@@ -0,0 +1,80 @@
+/**
+ * Lasius - Open source time tracker for teams
+ * Copyright (c) Tegonal Genossenschaft (https://tegonal.com)
+ *
+ * This file is part of Lasius.
+ *
+ * Lasius is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Lasius is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Lasius.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { useTranslation } from 'react-i18next'
+
+import { decimalHoursToDurationString } from '~/lib/utils/duration'
+
+type Props = {
+	bookings: number
+	expectedHours: number
+	fulfilledPercentage: number
+	hours: number
+	period?: 'day' | 'month' | 'week'
+}
+
+export const StatsOverviewGrid = ({
+	bookings,
+	expectedHours,
+	fulfilledPercentage,
+	hours,
+}: Props) => {
+	const { t } = useTranslation('common')
+
+	return (
+		<div className="flex-1 space-y-3">
+			<div className="stats grid w-full grid-cols-2">
+				<div className="stat">
+					<div className="stat-title">
+						{t('bookings.title', { defaultValue: 'Bookings' })}
+					</div>
+					<div className="stat-value">{bookings}</div>
+				</div>
+				<div className="stat">
+					<div className="stat-title">
+						{t('common.time.hours', { defaultValue: 'Hours' })}
+					</div>
+					<div className="stat-value">
+						{decimalHoursToDurationString(hours)}
+					</div>
+				</div>
+			</div>
+			<div className="stats grid w-full grid-cols-2">
+				<div className="stat">
+					<div className="stat-title">
+						{t('statistics.expectedHours', {
+							defaultValue: 'Expected hours',
+						})}
+					</div>
+					<div className="stat-value">
+						{decimalHoursToDurationString(expectedHours)}
+					</div>
+				</div>
+				<div className="stat">
+					<div className="stat-title">
+						{t('statistics.percentOfPlannedHours', {
+							defaultValue: '% of planned hours',
+						})}
+					</div>
+					<div className="stat-value">{fulfilledPercentage.toFixed(0)}%</div>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ frontend_rr7/app/features/dashboard/components/top-projects-card.tsx
@@ -0,0 +1,84 @@
+/**
+ * Lasius - Open source time tracker for teams
+ * Copyright (c) Tegonal Genossenschaft (https://tegonal.com)
+ *
+ * This file is part of Lasius.
+ *
+ * Lasius is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Lasius is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Lasius.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { useTranslation } from 'react-i18next'
+
+import { type ProjectSummary } from '~/lib/api/functions/aggregate-project-hours'
+
+type Props = {
+	emptyMessage: string
+	projects: ProjectSummary[]
+	showTopPrefix?: boolean
+}
+
+export const TopProjectsCard = ({
+	emptyMessage,
+	projects,
+	showTopPrefix = true,
+}: Props) => {
+	const { t } = useTranslation('common')
+
+	const title = showTopPrefix
+		? `Top ${t('projects.title', { defaultValue: 'Projects' })}`
+		: t('projects.title', { defaultValue: 'Projects' })
+
+	if (projects.length === 0) {
+		return (
+			<div className="flex-1">
+				<div className="stats h-fit w-full">
+					<div className="stat">
+						<div className="stat-title">{title}</div>
+						<div className="text-base-content/60 py-8 text-center text-sm">
+							{emptyMessage}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div className="flex-1">
+			<div className="stats h-fit w-full">
+				<div className="stat">
+					<div className="stat-title">{title}</div>
+					<div className="mt-2 space-y-2">
+						{projects.map((project) => (
+							<div className="flex flex-col gap-1" key={project.name}>
+								<div className="flex items-center justify-between text-sm">
+									<span className="flex-1 truncate font-medium">
+										{project.name}
+									</span>
+									<span className="text-base-content/60 ml-2 text-xs">
+										{project.hours.toFixed(1)}h
+									</span>
+								</div>
+								<progress
+									className="progress progress-primary h-2"
+									max="100"
+									value={project.percentage}
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
