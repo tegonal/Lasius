29979a29d7876093d52d7714ba12f079577e9227
Oliver Studer <oliver.studer@tegonal.com>
2026-03-21 16:33:29 +0100

    Add calendar navigation and selection hooks

---
+++ frontend_rr7/app/features/calendar/hooks/use-calendar-navigation.ts
@@ -0,0 +1,104 @@
+/**
+ * Lasius - Open source time tracker for teams
+ * Copyright (c) Tegonal Genossenschaft (https://tegonal.com)
+ *
+ * This file is part of Lasius.
+ *
+ * Lasius is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Lasius is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Lasius.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { addMonths, addWeeks } from 'date-fns'
+import { useCallback, useEffect, useState } from 'react'
+
+import { useCalendarActions } from '~/features/calendar/calendar-store'
+import {
+	formatISOLocale,
+	getMonthOfDate,
+	getWeekOfDate,
+	type IsoDateString,
+} from '~/lib/utils/dates'
+
+type ViewType = 'month' | 'week'
+
+export const useCalendarNavigation = (
+	initialDate: IsoDateString,
+	viewType: ViewType,
+) => {
+	const { setSelectedDate } = useCalendarActions()
+	const [period, setPeriod] = useState<IsoDateString[]>(
+		viewType === 'month'
+			? getMonthOfDate(initialDate)
+			: getWeekOfDate(initialDate),
+	)
+
+	// Update period when initialDate changes (e.g., when "Today" is clicked)
+	useEffect(() => {
+		const newPeriod =
+			viewType === 'month'
+				? getMonthOfDate(initialDate)
+				: getWeekOfDate(initialDate)
+		setPeriod(newPeriod)
+	}, [initialDate, viewType])
+
+	const next = useCallback(() => {
+		setPeriod((currentPeriod) => {
+			const firstDay = currentPeriod[0]
+			if (!firstDay) return currentPeriod
+			const currentFirst = new Date(firstDay)
+			const nextPeriod =
+				viewType === 'month'
+					? addMonths(currentFirst, 1)
+					: addWeeks(currentFirst, 1)
+			const nextPeriodFormatted = formatISOLocale(nextPeriod)
+			// Update selected date in store so stats update
+			setSelectedDate(nextPeriodFormatted)
+			return viewType === 'month'
+				? getMonthOfDate(nextPeriod)
+				: getWeekOfDate(nextPeriodFormatted)
+		})
+	}, [viewType, setSelectedDate])
+
+	const previous = useCallback(() => {
+		setPeriod((currentPeriod) => {
+			const firstDay = currentPeriod[0]
+			if (!firstDay) return currentPeriod
+			const currentFirst = new Date(firstDay)
+			const prevPeriod =
+				viewType === 'month'
+					? addMonths(currentFirst, -1)
+					: addWeeks(currentFirst, -1)
+			const prevPeriodFormatted = formatISOLocale(prevPeriod)
+			// Update selected date in store so stats update
+			setSelectedDate(prevPeriodFormatted)
+			return viewType === 'month'
+				? getMonthOfDate(prevPeriod)
+				: getWeekOfDate(prevPeriodFormatted)
+		})
+	}, [viewType, setSelectedDate])
+
+	const goToDate = useCallback(
+		(date: IsoDateString) => {
+			setPeriod(
+				viewType === 'month' ? getMonthOfDate(date) : getWeekOfDate(date),
+			)
+		},
+		[viewType],
+	)
+
+	return {
+		goToDate,
+		next,
+		period,
+		previous,
+	}
+}
---
+++ frontend_rr7/app/features/calendar/hooks/use-calendar-selection.ts
@@ -0,0 +1,75 @@
+/**
+ * Lasius - Open source time tracker for teams
+ * Copyright (c) Tegonal Genossenschaft (https://tegonal.com)
+ *
+ * This file is part of Lasius.
+ *
+ * Lasius is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Lasius is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Lasius.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { toDate } from 'date-fns'
+import { useEffect, useState } from 'react'
+
+import {
+	useCalendarActions,
+	useSelectedDate,
+} from '~/features/calendar/calendar-store'
+import { formatISOLocale, type IsoDateString } from '~/lib/utils/dates'
+
+/**
+ * Manages day selection with optional calendar store integration
+ */
+export const useCalendarSelection = (
+	initialDate: IsoDateString,
+	useStore = true,
+) => {
+	const storeSelectedDate = useSelectedDate()
+	const { setSelectedDate: setStoreSelectedDate } = useCalendarActions()
+
+	const [selectedDay, setSelectedDay] = useState<IsoDateString>(
+		useStore ? storeSelectedDate : initialDate,
+	)
+
+	useEffect(() => {
+		if (useStore) {
+			setSelectedDay(storeSelectedDate)
+		}
+	}, [storeSelectedDate, useStore])
+
+	const selectDay = (day: IsoDateString) => {
+		setSelectedDay(day)
+		if (useStore) {
+			setStoreSelectedDate(day)
+		}
+	}
+
+	const selectToday = () => {
+		selectDay(formatISOLocale(new Date()))
+	}
+
+	const getDay = (str: IsoDateString) => {
+		return toDate(new Date(str)).getDate()
+	}
+
+	const isDaySelected = (day: IsoDateString) => {
+		return getDay(selectedDay) === getDay(day)
+	}
+
+	return {
+		getDay,
+		isDaySelected,
+		selectDay,
+		selectedDay,
+		selectToday,
+	}
+}
